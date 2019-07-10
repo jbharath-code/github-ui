93182f6d3467ea0fc61f5e393cc9a8b74f1fc6f4
Bharath Janyavula <bharath.janyavula@knolskape.com>
2019-07-10 15:54:11 +0530

    reposiveness for 700px

--- src/app/components/githubDashbboard/userDetails/UserDetails.jsx
+++ src/app/components/githubDashbboard/userDetails/UserDetails.jsx
@@ -8,57 +8,59 @@ class UserDetails extends React.Component {
 		return (
 			<div className="user-block">
 				<img className="user-image" src={user.avatar_url} />
-				<div className="user-name-handle">
-					<div className="user-name">
-						{user.name}
+				<div className="user-info">
+					<div className="user-name-handle">
+						<div className="user-name">
+							{user.name}
+						</div>
+						<div className="user-handle">
+							{user.login}
+						</div>
 					</div>
-					<div className="user-handle">
-						{user.login}
+					<div className="user-follow">
+						Follow
 					</div>
-				</div>
-				<div className="user-follow">
-					Follow
-				</div>
-				<div>
-					<div className="user-bio">
-						{user.bio}
+					<div>
+						<div className="user-bio">
+							{user.bio}
+						</div>
+						<div className="user-company">
+							<span>
+								<img className="image-icon"
+									src="https://knolskape-website.s3.amazonaws.com/misc/bharath_janyavula/2019/07/10/29/friends.svg"
+								/>
+							</span>
+							<span>
+								{user.company}
+							</span>
+						</div>
+						<div className="user-location">
+							<span>
+								<img className="image-icon"
+									src="https://knolskape-website.s3.amazonaws.com/misc/bharath_janyavula/2019/07/10/49/placeholder.svg"
+								/>
+							</span>
+							<span>
+								{user.location}
+							</span>
+						</div>
+						{
+							user.email 
+							? 
+								<div className="user-email">
+									<span>
+										<img className="image-icon"
+											src="https://knolskape-website.s3.amazonaws.com/misc/bharath_janyavula/2019/07/10/71/mail.svg"
+										/>
+									</span>
+									<span>
+										{user.email}
+									</span>
+								</div>
+							:
+							null
+						}
 					</div>
-					<div className="user-company">
-						<span>
-							<img className="image-icon"
-								src="https://knolskape-website.s3.amazonaws.com/misc/bharath_janyavula/2019/07/10/29/friends.svg"
-							/>
-						</span>
-						<span>
-							{user.company}
-						</span>
-					</div>
-					<div className="user-location">
-						<span>
-							<img className="image-icon"
-								src="https://knolskape-website.s3.amazonaws.com/misc/bharath_janyavula/2019/07/10/49/placeholder.svg"
-							/>
-						</span>
-						<span>
-							{user.location}
-						</span>
-					</div>
-					{
-						user.email 
-						? 
-							<div className="user-email">
-								<span>
-									<img className="image-icon"
-										src="https://knolskape-website.s3.amazonaws.com/misc/bharath_janyavula/2019/07/10/71/mail.svg"
-									/>
-								</span>
-								<span>
-									{user.email}
-								</span>
-							</div>
-						:
-						null
-					}
 				</div>
 			</div>
 		);
